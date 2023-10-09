2aeced9efda009c004ecf3bad4e399558581713c
ikip1924 <rifkyrahman0420@gmail.com>
2023-10-10 00:07:55 +0700

    f: navigation action

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import { Open_Sans } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
 import { ThemeProvider } from '@/components/provider/provider-theme'
+import { ModalProvider } from '@/components/provider/modal-provider'
 import { cn } from '@/lib/utils'
 
 const font = Open_Sans({ subsets: ['latin'] })
@@ -27,6 +28,7 @@ export default function RootLayout({
 						enableSystem={false}
 						storageKey='discord-theme'
 					>
+						<ModalProvider />
 						{children}
 					</ThemeProvider>
 				</body>
--- components/navigation/navigation-action.tsx
+++ components/navigation/navigation-action.tsx
@@ -1,9 +1,11 @@
-"use-client"
+"use client"
 
 import { Plus } from "lucide-react"
 import { ActionTooltip } from "@/components/action-tooltip"
+import { useModal } from "@/hooks/use-modal-store"
 
-export const NavigationAction = async () => {
+export const NavigationAction = () => {
+	const { onOpen } = useModal()
 	return (
 		<div>
 			<ActionTooltip
@@ -11,7 +13,9 @@ export const NavigationAction = async () => {
 				align="center"
 				label="add a server"
 			>
-				<button className="flex items-center group">
+				<button
+					onClick={() => onOpen("createServer")}
+					className="flex items-center group">
 					<div className="flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden items-center justify-center bg-background dark:bg-neutral-700 group-hover:bg-emerald-500">
 						<Plus
 							className="transition group-hover:text-white text-emerald-500"
